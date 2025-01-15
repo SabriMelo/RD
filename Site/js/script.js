a3b167bc679bca14801f8a1215d7e1b975dafb47
SabriSouEu <sabrinabernardo002@gmail.com>
2025-01-15 14:08:55 -0300

    ajustes

---
+++ Site/js/script.js
@@ -0,0 +1,10 @@
+function filtrarProjetos(tipo) {
+    const projetos = document.querySelectorAll('.projeto');
+    projetos.forEach(projeto => {
+        if (tipo === 'todos' || projeto.classList.contains(tipo)) {
+            projeto.classList.add('visible');
+        } else {
+            projeto.classList.remove('visible');
+        }
+    });
+}
